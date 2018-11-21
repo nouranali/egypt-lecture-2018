441cb6305595f108f8c704510a7c0eeec3e3bd42
Nouran Ali <nouranalimohammed@gmail.com>
2018-11-21 21:06:24 +0200

    Update Json.scala

--- pre-talk-quiz/src/main/scala/com/abdulradi/json/Json.scala
+++ pre-talk-quiz/src/main/scala/com/abdulradi/json/Json.scala
@@ -1,5 +1,5 @@
 package com.abdulradi.json
-
+ 
 /**
   * Data structures that represents JSON in memory (http://json.org/)
   */
@@ -10,17 +10,16 @@ final case class JsonNumber(value: BigDecimal) extends Json
 final case class JsonBoolean(value: Boolean) extends Json
 final case class JsonArray(values: Seq[Json]) extends Json
 final case class JsonObject(values: Seq[(String, Json)]) extends Json
-
+ 
 object JsonSerialisation {
   /**
     * Converts Json instance to it's string representation
     */
   def serialise(json: Json): String = json match {
     case JsonNull => "null"
-    case JsonString(value) => ???
+    case JsonString(value) => "\"" + value + "\""
     case JsonNumber(value) => value.toString
-    case JsonBoolean(value) => ???
+    case JsonBoolean(value) => value.toString
     case JsonArray(values) => values.map(v => serialise(v)).mkString("[", ",", "]")
-    case JsonObject(values) => ???
-  }
+    case JsonObject(values) => values.map(s => "\""+ s._1+ "\""+":"+ serialise(s._2)).mkString("{",",","}")}
 }
